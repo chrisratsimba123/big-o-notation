721916cd5af86e7b751a22f579f82ccccdb207f1
Chris Ratsimbazafy <ratsimbazafy@Chriss-MacBook-Air.local>
2020-10-01 12:07:31 -0400

    added counter function

--- timing.js
+++ timing.js
@@ -5,6 +5,7 @@ function addUpTo(n) {
     for (let i = 1; i <= n; i++) {
         total += i
     }
+    // Because this is in a loop, this has 'n' additions and 'n' assignments
     return total
 }
 
@@ -16,6 +17,7 @@ console.log(`Time Elapsed: ${(t2 - t1) / 1000} seconds`)
 // Approach #2
 
 function addUpTo(n) {
+    // Only 3 operations here
     return n * (n + 1) / 2
 }
 
@@ -23,3 +25,18 @@ let time1 = performance.now()
 addUpTo(1000000000)
 let time2 = performance.now()
 console.log(`Time Elapsed: ${(t2 - t1) / 1000} seconds`)
+
+// Counting Function
+
+function countUpAndDown(n) {
+    console.log("Going up!")
+    for (let i = 0; i < n; i++) {
+        console.log(i)
+    }
+    console.log("At the top! \nGoing down...")
+    for (let j = n - 1; j >= 0; j--) {
+        console.log(j)
+    }
+    console.log("Back down. Bye!")
+}
+
